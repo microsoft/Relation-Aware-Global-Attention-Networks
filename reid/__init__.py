9b85bc3c1ab262bb986d36a375ae0da8cb51d5b8
Cuiling Lan <52407176+lcl-2019@users.noreply.github.com>
2020-04-13 20:55:26 +0800

    Add files via upload

---
+++ reid/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from . import evaluation_metrics
+from . import loss
+from . import models
+from . import utils
+from . import img_trainers
+from . import img_evaluators
+
+__version__ = '0.2.0'
