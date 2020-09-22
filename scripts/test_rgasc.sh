cbe5a463e387d6fabaf61382f5c0008149c9eed2
Toraudonn <toraudonn@gmail.com>
2020-09-22 10:28:29 +0000

    add test script

--- scripts/test_rgasc.sh
+++ scripts/test_rgasc.sh
@@ -7,7 +7,7 @@ BATCH_SIZE=64
 NUM_WORKER=8
 NUM_FEATURE=2048
 NUM_EPOCHS=600
-NUM_GPU=2
+NUM_GPU=1
 SEED=16
 START_SAVE=320
 BRANCH_NAME="rgasc"
@@ -17,38 +17,21 @@ DATA_DIR="${HOME}/data"
 LOG_DIR="./logs/RGA-SC/cuhk03labeled_b64f2048"
 LOG_FILE="${LOG_DIR}/train_log.txt"
 
-if [ -d ${LOG_DIR} ]; then
-	rm -r ${LOG_DIR}
-fi
-
 if [ ! -d ${LOG_DIR} ]; then
 	echo ${LOG_DIR}" not exists!!!"
-	mkdir -p ${LOG_DIR}
+	exit 1
 fi
-
-echo "Begin to train."
-CUDA_VISIBLE_DEVICES=2,3, python main_imgreid.py \
-	-a ${NAME_MODEL} \
-	-b ${BATCH_SIZE} \
-	-d ${NAME_DATA} \
-	-j ${NUM_WORKER} \
-	--opt adam \
-	--dropout 0 \
-	--combine-trainval \
-	--seed ${SEED} \
-	--num_gpu ${NUM_GPU} \
-	--epochs ${NUM_EPOCHS} \
-	--features ${NUM_FEATURE} \
-	--start_save ${START_SAVE} \
-	--branch_name ${BRANCH_NAME} \
-	--data-dir ${DATA_DIR} \
-	--logs-dir ${LOG_DIR} \
-	> ${LOG_FILE} 2>&1
 
 echo "Begin to test."
 WEIGHT_FILE="${LOG_DIR}/checkpoint_${CKPT}.pth.tar"
 LOG_FILE_TEST="${LOG_DIR}/eval_${CKPT}.txt"
-CUDA_VISIBLE_DEVICES=2,3 python main_imgreid.py \
+
+if [ ! -f ${WEIGHT_FILE} ]; then
+	echo ${WEIGHT_FILE}" not exists!!!"
+	exit 1
+fi
+
+CUDA_VISIBLE_DEVICES=0 python main_imgreid.py \
 	-a ${NAME_MODEL} \
 	-b ${BATCH_SIZE} \
 	-d ${NAME_DATA} \
